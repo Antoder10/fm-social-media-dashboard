6a4743e7a0267534d7a727bf7fd961708ee53146
Antonio Deriu <antoderiuweb@gmail.com>
2021-03-27 18:27:43 +0100

    ui fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: {
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    options: {
+      safelist: ['border-facebook', 'border-twitter', 'border-instagram', 'border-youtube']
+    },
+  },
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
