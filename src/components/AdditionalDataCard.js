4575dd7a129d157390b7a9ff27690408800e1c95
Antonio Deriu <antoderiuweb@gmail.com>
2021-03-27 17:43:11 +0100

    ui fix

--- src/components/AdditionalDataCard.js
+++ src/components/AdditionalDataCard.js
@@ -12,7 +12,7 @@ const replaceNegativeSign = text => text.replace('-', '');
 const AdditionalDataCard = ({additionalData}) => {
 
   return (
-    <div className="flex flex-col bg-light-grayish-blue hover:bg-very-pale-blue p-10 rounded-md shadow-md cursor-pointer dark:hover:bg-desaturated-blue dark:bg-dark-desaturated-blue">
+    <div className="flex flex-col mb-8 sm:mb-0 bg-light-grayish-blue hover:bg-very-pale-blue p-10 rounded-md shadow-md cursor-pointer dark:hover:bg-desaturated-blue dark:bg-dark-desaturated-blue">
       <div className="flex flex-row mb-6 items-center justify-between">
         <span className="font-bold text-dark-grayish-blue">{Object.keys(additionalData)[1]}</span>
         <img src={`/images/icon-${additionalData.name.toLowerCase()}.svg`} alt={`icon-${additionalData.name}`} className="ml-auto w-6"/>
